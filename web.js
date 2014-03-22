29d4113db2b4a11aa920ac8fb4bc737e0f80eeb0
seosan <lithrome@gmail.com>
2014-03-22 20:54:32 +0900

    :a:

--- web.js
+++ web.js
@@ -8,6 +8,12 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
   res.sendfile('/usr/index.html', {root:__dirname});
 });
+app.get('/js.js', function(req, res) {
+  res.sendfile('/usr/js.js', {root:__dirname});
+});
+/*app.get('/', function(req, res) {
+  res.sendfile('/usr/css.style', {root:__dirname});
+});*/
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
