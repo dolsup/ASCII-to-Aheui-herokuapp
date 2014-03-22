b3d854f58e3692988488a872a02827d4f5552978
seosan <lithrome@gmail.com>
2014-03-22 20:29:28 +0900

    b

--- web.js
+++ web.js
@@ -1,28 +1,16 @@
-// web.js
-
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-// ---------------------------------------
-
 
 app.get('/', function(req, res) {
   res.sendfile('/usr/index.html', {root:__dirname});
 });
-/*app.get('/css', function(req, res) {
-  res.sendfile('/usr/style.css', {root:__dirname});
-});
-app.get('/js', function(req, res) {
-  res.sendfile('/usr/js.js', {root:__dirname});
-});*/
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-// ---------------------------------------
-
 
