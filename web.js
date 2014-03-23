a56652ff652840ab68ccca65781244fea98845bb
seosan <lithrome@gmail.com>
2014-03-23 09:33:23 +0900

    a

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-
+var cnt = 0;
 app.use(logfmt.requestLogger());
 
 
 app.get('/', function(req, res) {
   res.sendfile('/usr/index.html', {root:__dirname});
+  console.log(++cnt);
 });
 app.get('/js.js', function(req, res) {
   res.sendfile('/usr/js.js', {root:__dirname});
